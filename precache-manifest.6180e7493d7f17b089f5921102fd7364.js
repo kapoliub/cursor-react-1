b4978a350c3588bf49ffc7f1ee4d0b7d8441e4dd
Liubomyr <louidropson@gmail.com>
2020-09-02 11:39:06 +0300

    Updates

---
+++ precache-manifest.6180e7493d7f17b089f5921102fd7364.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502c37389023324c1e3b10bf2baca35f",
+    "url": "/cursor-react-1/index.html"
+  },
+  {
+    "revision": "b2acb448aed6ab3b49e2",
+    "url": "/cursor-react-1/static/css/main.4c82eb56.chunk.css"
+  },
+  {
+    "revision": "e3c3d6e5bed4fe53dcb5",
+    "url": "/cursor-react-1/static/js/2.46b56a5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cursor-react-1/static/js/2.46b56a5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2acb448aed6ab3b49e2",
+    "url": "/cursor-react-1/static/js/main.01f38c27.chunk.js"
+  },
+  {
+    "revision": "c2b98f4d8e758ca119d4",
+    "url": "/cursor-react-1/static/js/runtime-main.801b01f3.js"
+  },
+  {
+    "revision": "1cd146fe68ce9aae34927692e4110d28",
+    "url": "/cursor-react-1/static/media/arrow-logo.1cd146fe.svg"
+  },
+  {
+    "revision": "6344ee7bdd3f87d30add1b9a5f1422f0",
+    "url": "/cursor-react-1/static/media/comment_icon.6344ee7b.svg"
+  },
+  {
+    "revision": "57c9a039101ba4c5d3e5a7cc8b541f52",
+    "url": "/cursor-react-1/static/media/like_icon.57c9a039.svg"
+  },
+  {
+    "revision": "a9a99fc3841f1690924d6d94a37bc78e",
+    "url": "/cursor-react-1/static/media/repost_icon.a9a99fc3.svg"
+  },
+  {
+    "revision": "8e82847c0b2cfcb461c4dadefb5b59d0",
+    "url": "/cursor-react-1/static/media/share_icon.8e82847c.svg"
+  },
+  {
+    "revision": "2019cadf6bdc5d95f29a232dff10f6ea",
+    "url": "/cursor-react-1/static/media/verified-logo.2019cadf.svg"
+  }
+]);
